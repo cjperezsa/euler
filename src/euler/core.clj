093719ccb9ed5de400997527fb461e4d7ba01f06
cperez <carlos.perez.sanjuan@gmail.com>
2012-12-21 13:27:55 +0100

    Problemas 1 y 2 resueltos

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -1,6 +1,11 @@
 (ns euler.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn problem-1
+  "Find the sum of all the multiples of 3 or 5 below n"
+  [n]
+  (reduce + (filter #(or (zero? (rem % 3)) (zero? (rem % 5))) (take n (iterate inc 0)))))
+
+(defn problem-2
+  "By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms."
+  [n]
+  (reduce + (filter #(even? %) (take-while (partial >= n) (map first (iterate (fn [[a b]] [b (+ a b)]) [0N 1N]))))))
